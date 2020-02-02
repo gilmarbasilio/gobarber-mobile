83668a748bbec95cc530ef32fe71589f0a36eaf6
GIlmar Basilio <gilmar.basilio@gmail.com>
2020-02-02 08:15:43 -0300

    starting flux of appointment

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/sign-in/SignIn';
 import SignUp from './pages/sign-up/SignUp';
 
 import Dashboard from './pages/dashboard/Dashboard';
 import Profile from './pages/profile/Profile';
+
+import SelectProvider from './pages/new/select-provider /SelectProvider';
+import SelectDateTime from './pages/new/select-date-time/SelectDateTime';
+import Confirm from './pages/new/confirm/Confirm';
 
 export default (signedIn = false) =>
   createAppContainer(
@@ -18,6 +26,35 @@ export default (signedIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
